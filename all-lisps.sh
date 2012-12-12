9388000bc9f0d40ce542341e93001f2c9ee2182f
John Lapeyre <jlapeyre@cpan.org>
2012-12-12 14:31:15 +0100

    modified:   all-lisps.sh 	modified:   testall.mac

--- all-lisps.sh
+++ all-lisps.sh
@@ -13,8 +13,8 @@
 # lisp implementations. Allegro worked with an earlier version
 # of this software, as well.
 
-#maximas="smaxima gmaxima clmaxima emaxima cmumaxima ccmaxima"
-maximas="smaxima"
+maximas="smaxima gmaxima clmaxima emaxima cmumaxima ccmaxima"
+#maximas="smaxima"
 
 # Build just the mext_system
 build_mext () {
