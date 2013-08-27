3fc716d6612bc3ba1c7d906826a216dfbda45d68
John Lapeyre <jlapeyre@cpan.org>
2013-08-27 22:09:45 +0200

    modified:   INSTALL 	renamed:    INSTALL -> INSTALL-windows.txt 	modified:   buildall.mac 	deleted:    mext_system/mext-ibuild.lisp

--- mext_system/mext-ibuild.lisp
+++
@@ -1,9 +0,0 @@
-;; load this to build the mext_system distribution
-
-(let ((file (make-pathname :name "load_mext_maxima" :type "lisp"
-         :defaults  #-gcl *load-pathname* #+gcl sys:*load-pathname*)))
-  (load file))
-($mext_dist_clean)
-($mext_dist_build)
-($mext_dist_user_install)
-($mext_dist_clean)
