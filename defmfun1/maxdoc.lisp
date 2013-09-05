9ea66157a01dbf7e9094d2d9d162790949c3078c
John Lapeyre <jlapeyre@cpan.org>
2013-09-05 11:19:49 +0200

    Moved markup keys to a separate file 	modified:   defmfun1/defmfun1.mac 	modified:   defmfun1/defmfun1.system 	modified:   defmfun1/maxdoc.lisp

--- defmfun1/maxdoc.lisp
+++ defmfun1/maxdoc.lisp
@@ -155,34 +155,33 @@
 ;;; :arg -- argument to function
 ;;; :var -- other variable
 
-
-(fill-format-codes *format-codes-text*
-   '( (:code "`~a'")  (:codedot "`~a'.")  (:codecomma "`~a',") 
-      (:mref "`~a'")  (:mrefdot "`~a'.")  (:mrefcomma "`~a',") 
-      (:emref "`~a'")  (:emrefdot "`~a'.")  (:emrefcomma "`~a',") 
-      (:arg "<~a>")   (:argdot "<~a>.")   (:argcomma "<~a>,")
-      (:var "<~a>")   (:vardot "<~a>.")   (:varcomma "<~a>,")
-      (:opt "<~a>")   (:optdot "<~a>.")   (:optcomma "<~a>,")
-      (:par "~%~%~a")
-      (:dquote "\"~a\"") (:dquotedot "\"~a\".") (:dquotecomma "\"~a\",")
-      (:dcode "~%~%`~a'~%~%")
-      (:math "~a") (:tmath "~a") (:lif "~a")
-      (:dmath "~a") (:dots " ... ")))
+;; (fill-format-codes *format-codes-text*
+;;    '( (:code "`~a'")  (:codedot "`~a'.")  (:codecomma "`~a',") 
+;;       (:mref "`~a'")  (:mrefdot "`~a'.")  (:mrefcomma "`~a',") 
+;;       (:emref "`~a'")  (:emrefdot "`~a'.")  (:emrefcomma "`~a',") 
+;;       (:arg "<~a>")   (:argdot "<~a>.")   (:argcomma "<~a>,")
+;;       (:var "<~a>")   (:vardot "<~a>.")   (:varcomma "<~a>,")
+;;       (:opt "<~a>")   (:optdot "<~a>.")   (:optcomma "<~a>,")
+;;       (:par "~%~%~a")
+;;       (:dquote "\"~a\"") (:dquotedot "\"~a\".") (:dquotecomma "\"~a\",")
+;;       (:dcode "~%~%`~a'~%~%")
+;;       (:math "~a") (:tmath "~a") (:lif "~a")
+;;       (:dmath "~a") (:dots " ... ")))
 
 ;; mref, mrefdot, mrefcomma below are not used. caught in earlier branch
 ;; emref etc. should eventually do external links
-(fill-format-codes *format-codes-latex*
-   '( (:code "{\\tt ~a}")  (:codedot "{\\tt ~a}.")  (:codecomma "{\\tt ~a},") 
-      (:mref "{\\tt ~a}")  (:mrefdot "{\\tt ~a}.")  (:mrefcomma "{\\tt ~a},")
-      (:emref "{\\tt ~a}")  (:emrefdot "{\\tt ~a}.")  (:emrefcomma "{\\tt ~a},")
-      (:arg "{\\it ~a}")   (:argdot "{\\it ~a}.")   (:argcomma "{\\it ~a},")
-      (:var "{\\it ~a}")   (:vardot "{\\it ~a}.")   (:varcomma "{\\it ~a},")
-      (:opt "{\\it ~a}")   (:optdot "{\\it ~a}.")   (:optcomma "{\\it ~a},")
-      (:dquote "``~a''") (:dquotedot "``~a''.") (:dquotecomma "``~a'',")
-      (:par "~%~%~%~a")
-      (:math "$~a$") (:tmath "$~a$") (:lif "~a")
-      (:dcode "~%\\begin{verbatim}~%~a~%\\end{verbatim}~%~%")
-      (:dmath "~%$$~a$$~%") (:dots "\\ldots")))
+;; (fill-format-codes *format-codes-latex*
+;;    '( (:code "{\\tt ~a}")  (:codedot "{\\tt ~a}.")  (:codecomma "{\\tt ~a},") 
+;;       (:mref "{\\tt ~a}")  (:mrefdot "{\\tt ~a}.")  (:mrefcomma "{\\tt ~a},")
+;;       (:emref "{\\tt ~a}")  (:emrefdot "{\\tt ~a}.")  (:emrefcomma "{\\tt ~a},")
+;;       (:arg "{\\it ~a}")   (:argdot "{\\it ~a}.")   (:argcomma "{\\it ~a},")
+;;       (:var "{\\it ~a}")   (:vardot "{\\it ~a}.")   (:varcomma "{\\it ~a},")
+;;       (:opt "{\\it ~a}")   (:optdot "{\\it ~a}.")   (:optcomma "{\\it ~a},")
+;;       (:dquote "``~a''") (:dquotedot "``~a''.") (:dquotecomma "``~a'',")
+;;       (:par "~%~%~%~a")
+;;       (:math "$~a$") (:tmath "$~a$") (:lif "~a")
+;;       (:dcode "~%\\begin{verbatim}~%~a~%\\end{verbatim}~%~%")
+;;       (:dmath "~%$$~a$$~%") (:dots "\\ldots")))
 
 (defun make-texi-codes (table codes)
  "Make a table of symbols to codes for texi. E.g. var --> @var{~a}"
