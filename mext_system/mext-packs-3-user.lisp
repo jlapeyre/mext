51ded005a153cb8c049e1935796eaa71279b9169
John Lapeyre <jlapeyre@cpan.org>
2013-09-09 15:19:45 +0200

    add different levels of loading mext, mext1, mext2,...

---
+++ mext_system/mext-packs-3-user.lisp
@@ -0,0 +1,19 @@
+;; This file is copied to the user's maxima directory and
+;; renamed: mext_core.lisp.  In fact, this message will be
+;; visible. It may be the first file loaded to get
+;; both the mext system and core packages. See below.
+
+;; Only load mext once. Need this for gcl,
+;; because, in gcl, loading mext always resets *default-pathname-defaults* to
+;; whatever (truename ".") gives at the moment.
+;; Load mostly the main packages, few repackaged third party things.
+;; This file will be the first mext file loaded by the user:
+;; e.g. load(mext_core);
+;; The minimum mext system is loaded with load(mext).
+
+(when (not (find-package :mext-maxima-load))
+  ($load "mext_load.lisp"))
+
+(loop :for mext-package :in '( "lists_aex" "discrete_aex" "numerical" "runtime"
+                               "alt_eigen" "tpsolve" ) :do 
+      ($require mext-package))
