e7da6bbbeafecfe924b0c99dbd04ac1f4abf67dd
John Lapeyre <jlapeyre@cpan.org>
2013-09-10 22:08:55 +0200

    Didnt need this, i think. 	deleted:    mydiagnostic.lisp

--- mydiagnostic.lisp
+++
@@ -1,7 +0,0 @@
-(in-package :maxima)
-
-(defmfun $show_infolists ()
-  (loop for list in 
-        `($values $functions $macros $arrays $myoptions $props $aliases 
-                   $rules $gradefs $dependencies $let_rule_packages $structures) do
-                   (format t "  ~a:~% ~a~%" list ($sconcat (eval list)))))
