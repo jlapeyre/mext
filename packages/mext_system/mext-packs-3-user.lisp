be0281bcf7fcb6c639eaac87c638a2dc2ad97470
John Lapeyre <jlapeyre@cpan.org>
2013-09-10 23:36:04 +0200

    update comments 	modified:   packages/mext_system/mext-packs-1-user.lisp 	modified:   packages/mext_system/mext-packs-2-user.lisp 	modified:   packages/mext_system/mext-packs-3-user.lisp

--- packages/mext_system/mext-packs-3-user.lisp
+++ packages/mext_system/mext-packs-3-user.lisp
@@ -1,5 +1,5 @@
 ;; This file is copied to the user's maxima directory and
-;; renamed: mext_core.lisp.  In fact, this message will be
+;; renamed: mext3.lisp.  In fact, this message will be
 ;; visible. It may be the first file loaded to get
 ;; both the mext system and core packages. See below.
 
