819e13d027fb975366e0577308c1115bb29327c5
John Lapeyre <jlapeyre@cpan.org>
2013-09-22 20:31:19 +0200

    quiet warning

--- packages/mext_system/fix-tex.lisp
+++ packages/mext_system/fix-tex.lisp
@@ -3,6 +3,7 @@
 
 ;; GJL -- copied from imaxima.lisp by
 ;; Jesper Harder, Yasuaki Honda
+(mext::no-warning
 (defun verb-quote (str)
   (let ((var "") (charlist
 		  '((#\Newline . "| \\\\ \\verb| "))))
@@ -11,12 +12,13 @@
 	(setq var (concatenate 'string var 
 			       (or (cdr (assoc chari charlist :test #'eql))
 				   (string chari))))))
-  var))
+  var)))
 
 ;; GJL -- hacked from src/mactex.lisp by RJF to
 ;; print verbatim objects that we don't really know how to print:
 ;; arrays, structs, etc.
-(defun tex-atom (x l r)
+(mext::no-warning
+(defun old-tex-atom (x l r)
   (append l
 	  (list (cond ((numberp x) (texnumformat x))
 		      ((and (symbolp x) (or (get x 'texword) (get (get x 'reversealias) 'texword))))
@@ -26,11 +28,12 @@
                       ((not ($mapatom x))
                        (verb-quote ($sconcat x)))
 		      (t (tex-stripdollar (or (get x 'reversealias) x)))))
-	  r))
+	  r)))
 
-(defvar *old-tex-atom* #'tex-atom)
+;(defvar *old-tex-atom* #'tex-atom)
 
 ;; GJL -- hacked from imaxima.lisp
+(mext::no-warning
 (defun tex-atom (x l r &aux other-case)
   (let ((result (append l
 			(list (cond ((mstringp x) (texstring x))
@@ -42,5 +45,19 @@
             (progn
               (setf x (list (verb-quote ($sconcat x))))
               (tex-list x l r 'mparen))
-            (funcall *old-tex-atom* x l r))
-      result)))
+;            (funcall *old-tex-atom* x l r))
+            (old-tex-atom x l r))
+      result))))
+
+(mext::no-warning
+(defun texstring (x)
+  (let ((sym-name
+	 (if (symbolp x)
+	     (print-case-sensitive x)
+	   x)))
+    (cond ((equal sym-name "") "")
+	  ((eql (elt sym-name 0) #\\) sym-name)
+	  ((memq (elt sym-name 0) '(#\$ #\&))
+	   (setq sym-name (subseq sym-name 1))
+	   (concatenate 'string "\\verb|   " (verb-quote sym-name) "|"))
+	  (t (concatenate 'string "\\verb|" (verb-quote sym-name) "|"))))))
