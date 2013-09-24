52bb2c7ac1ee8d442b87428e9555985a6e7815ee
John Lapeyre <jlapeyre@cpan.org>
2013-09-24 15:19:07 +0200

    add quicklisp function

--- packages/quicklisp/max-ql.lisp
+++ packages/quicklisp/max-ql.lisp
@@ -2,14 +2,14 @@
 (max-doc:set-cur-sec 'max-doc::quicklisp-fandv)
 (defmfun1:set-file-and-package "max-ql.lisp" "quicklisp")
 
-(defmfun1 ($quickload :doc) ((package_name :string :thread)
+(defmfun1 ($quickload :doc) ((dist_name :string :thread)
                              &opt ($verbose nil :bool))
-  :desc ("Load the asdf lisp package " :arg "package_name"
+  :desc ("Use quicklisp to load the distribution " :arg "dist_name"
          ", or, if not installed, install from the internet and then load.")
-  (quicklisp-client:quickload package_name :verbose $verbose)
+  (quicklisp-client:quickload dist_name :verbose $verbose)
   '$done)
 
-(defmfun1 ($quicklisp_search :doc) ( (term :string) )
+(defmfun1 ($quicklisp_search :doc) ((term :string))
   :desc 
   ("Search quicklisp for lisp `systems' (packages) matching " :argdot "term"
   " The empty string returns all avaliable systems.")
@@ -28,3 +28,13 @@
    available.")
   (quicklisp-client:update-all-dists)
   '$done)
+
+(defmfun1 ($quicklisp_update_dist :doc) ((dist_name :string :thread))
+  :desc
+  ("Update the distribution " :argdot "dist_name"
+   " This checks if a newer version is available and downloads and installs it."
+   " Or maybe this only works if the argument is " :codecomma "quicklisp"
+   " which updates quicklips itself.")
+  (ql:update-dist dist_name)
+  '$done)
+
